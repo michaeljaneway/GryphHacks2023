88afd816c31f70343cfbd02ea5a8063adcd36d93
Isaiah Sinclair <isaiah.sinclair.0711@gmail.com>
2023-05-27 11:21:35 -0400

    Added ability to send notes (list of instance variables) to AnimationGenerator class.

--- AnimationGenerator.py
+++ AnimationGenerator.py
@@ -1,8 +1,16 @@
 from manim import *
+from manim.camera.camera import Camera
+from ProjectStructs import Note
 
 # This is the class that generates an animation based on file input (audio and JSON)
 
 POINT_SIZE = 0.08
+NOTE_WIDTH = 5
+
+"""
+Note about notes:
+Note will hold frequency, key (actual note being played)
+"""
 
 class AnimationGenerator(Scene):
     def construct(self):
@@ -24,8 +32,14 @@ class AnimationGenerator(Scene):
         self.anchorLine = Line(self.anchorPoints[0], self.anchorPoints[1])
         self.anchorLine.color = RED
         
-        self.add(self.anchorLine)
+        self.topCircle = Circle().surround(anchorP1, buffer_factor=4)
+        self.topCircle.set_stroke(color=PINK)
         
+        self.add(self.anchorLine)
+        self.add(self.topCircle)
+        
+    def addNotes(self, notes):
+        self.notes = notes
     
     def initNotes(self, numNotes):
         self.noteCircles = []
@@ -33,13 +47,22 @@ class AnimationGenerator(Scene):
         for i in range(numNotes):
             self.noteCircles.append(Circle())
     
-            self.noteCircles[i].set_stroke(color=PINK, width=20)
-            self.add(self.noteCircles[i])
+            self.noteCircles[i].set_stroke(color=PINK, width=NOTE_WIDTH)
+            # self.add(self.noteCircles[i])
     
     def startPlaying(self):
         for i in self.noteCircles:
             pass
     
 if __name__ == "__main__":
+    # creating notes to test
+    notes = []
+    
+    for i in range(5):
+        # frequency right now is half of the note
+        noteToAdd = Note(i/2, i)
+        notes.append(noteToAdd)
+    
     scene = AnimationGenerator()
+    scene.addNotes(notes)
     scene.render()
