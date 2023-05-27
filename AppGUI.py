db1cf62aff5cecf1849233f5793496c5bbac9eee
Michael Janeway <mjaneway@uoguelph.ca>
2023-05-27 16:12:43 -0400

    GUI

--- AppGUI.py
+++ AppGUI.py
@@ -2,28 +2,63 @@ import tkinter as tk
 from tkinter import ttk
 import sv_ttk
 import pathlib
+from tkinter.filedialog import *
 
 
 class MainApplication(tk.Tk):
     def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
         super().__init__(screenName, baseName, className, useTk, sync, use)
 
+        sv_ttk.use_dark_theme()
+
         self.title("The Sounds of the Future")
 
-        # https://github.com/rdbende/Sun-Valley-ttk-theme
-
-        # All of these previous lines of code are essentially setting up the frame and window, and how they dynamically resize
+        # theme used: https://github.com/rdbende/Sun-Valley-ttk-theme
+        self.create_frames()
+        self.create_buttons()
+        self.create_labels()
+        self.create_spinboxes()
+        self.create_instrumentframe()
+        self.create_effectframe()
+        self.create_noteframe()
+        
+    def create_spinboxes(self):
+        #spinbox for the note # & octave
+        self.spinval = tk.StringVar(value="0")
+        self.keyselector = ttk.Spinbox(
+            self.framemid, from_=0.0, to=127.0, textvariable=self.spinval)
+        self.keyselector.grid(column=1, row=1, sticky=(
+            tk.N, tk.W, tk.E, tk.S), padx=10, pady=10)
+        
+        #trace the spinbox value of the note # and octave
+        self.spinval.trace("w", lambda x, y, z: self.show_key())
+        #spinbox for creating the velocity
+        self.velocityval = tk.StringVar(value="0")
+        self.velocitybox = ttk.Spinbox(
+            self.framemid, from_=0.0, to=127.0, textvariable=self.velocityval)
+        self.velocitybox.grid(column=1, row=3, sticky=(
+            tk.N, tk.W, tk.E, tk.S), padx=10, pady=10)
+        
+    def create_frames(self):
+        #creating the left frame
         self.frameleft = ttk.LabelFrame(
             self, padding="20", width=150, height=200, text="FrameLEFT")
         self.frameleft.grid(column=0, row=0, sticky=(
             tk.N, tk.S, tk.W, tk.E), padx=15, pady=15)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        # p = ttk.Progressbar(self.frameleft, orient=tk.HORIZONTAL, length=200, mode='determinate')
-        # p.grid(column=0,row=1,sticky=(tk.W,tk.E),padx=20,pady=20)
-        # p.start()
-        sv_ttk.use_dark_theme()
-        note_count = 1
+        
+        
+        self.testbutton1 = ttk.Button(self.frameleft,text="Hide Mid",command=lambda: self.hide_mid_frame())
+        self.testbutton2 = ttk.Button(self.frameleft,text="Show Mid",command=lambda: self.show_mid_frame())
+        self.testbutton1.grid(column=0, row=100)
+        self.testbutton2.grid(column=0, row=101)
+        
+        # Starts numbers at 1 
+        self.effect_count = 1
+        self.instrument_count = 1
+        self.note_count = 1
+        #creating the instrument tree on the right
         self.instrument_tree = ttk.Treeview(
             self)
         self.instrument_tree.grid(column=2, row=0, sticky=(
@@ -42,62 +77,88 @@ class MainApplication(tk.Tk):
 
         self.instrument_tree.bind(
             "<<TreeviewSelect>>", lambda event: self.tree_select())
-
-        self.framemid = ttk.LabelFrame(
-            self, padding="20", width=150, height=200, text="FrameMID")
+        
+    def create_instrumentframe(self):
+        #Instrument Frame Construction
+        self.frameinstrument = ttk.LabelFrame(
+        self, padding="20", width=150, height=200, text="FrameINSTRUMENT")
         self.framemid.grid(column=1, row=0, sticky=(
             tk.N, tk.E, tk.S, tk.W), padx=15, pady=15)
+        
+    def create_noteframe(self):
+        #Note frame construction
+        self.framenote = ttk.LabelFrame(
+            self, padding="20", width=150, height=200, text="FrameNOTE")
+        self.framemid.grid(column=1, row=0, sticky=(
+            tk.N, tk.E, tk.S, tk.W), padx=15, pady=15)
+        
+    def create_effectframe(self):
+        #Effect frame construction
+        self.frameeffect = ttk.LabelFrame(
+            self, padding="20", width=150, height=200, text="FrameEFFECT")
+        self.framemid.grid(column=1, row=0, sticky=(
+            tk.N, tk.E, tk.S, tk.W), padx=15, pady=15)
+        
+    def create_buttons(self):
+        #add an instrument Button
+        self.add_instrument_button = ttk.Button(self.frameleft,text="Add Instrument",command=lambda: self.add_instrument())
+        self.add_instrument_button.grid(column=0,row=0,sticky=(tk.N,tk.W,tk.E,tk.S),padx=10,pady=10)
 
-        self.spinval = tk.StringVar(value="0")
-        self.keyselector = ttk.Spinbox(
-            self.framemid, from_=0.0, to=127.0, textvariable=self.spinval)
-        self.keyselector.grid(column=1, row=1, sticky=(
-            tk.N, tk.W, tk.E, tk.S), padx=10, pady=10)
-
-        self.spinval.trace("w", lambda x, y, z: self.show_key())
-
-        self.keyselector_text = ttk.Label(
-            self.framemid, text="Note Key", justify="center")
-        self.keyselector_text.grid(column=1, row=0)
-
+        #button for rendering audio
+        self.render_button = ttk.Button(
+            self.frameleft, text="Render Audio", command=lambda: self.render())
+        self.render_button.grid(column=0, row=3, sticky=(
+            tk.E, tk.W), padx=50, pady=66)
+        
+        #delete a note Button
+        self.delete_button = ttk.Button(
+            self.frameleft, text="Delete Selected", command=lambda: self.delete_note())
+        self.delete_button.grid(column=0, row=1, sticky=(
+            tk.E, tk.W), padx=10, pady=10)
+        
+        #add a note Button
         self.Add_Note_button = ttk.Button(
             self.frameleft, text="Add Note", command=lambda: self.add_note()
         )
         self.Add_Note_button.grid(column=2, row=0, sticky=(
             tk.N, tk.W, tk.E, tk.S), padx=10, pady=10)
 
-        delete_button = ttk.Button(
-            self.frameleft, text="Delete Note", command=lambda: self.delete_note())
-        delete_button.grid(column=0, row=0, sticky=(
-            tk.E, tk.W), padx=10, pady=10)
 
-        render_button = ttk.Button(
-            self.framemid, text="Render Audio", command=lambda: self.render())
-        render_button.grid(column=0, row=3, sticky=(
-            tk.E, tk.W), padx=50, pady=66)
+        # self.countryvar = tk.StringVar()
+        # self.country = ttk.Combobox(self.<WHICHFRAME?>, textvariable=self.countryvar)
+        # self.country.grid(column=0,row=1)
 
-        self.velocityval = tk.StringVar(value="0")
-        self.velocitybox = ttk.Spinbox(
-            self.framemid, from_=0.0, to=127.0, textvariable=self.velocityval)
-        self.velocitybox.grid(column=1, row=3, sticky=(
-            tk.N, tk.W, tk.E, tk.S), padx=10, pady=10)
-
+    def create_labels(self):
+        #textbox for the velocity of the note
         self.velocitybox_text = tk.Label(
-            self.framemid, text="Velocity", justify="center")
+            self.framenote, text="Velocity", justify="center")
         self.velocitybox_text.grid(column=1, row=2)
 
-        # tk.Text.insert()
+        #key label
+        self.keyselector_text = ttk.Label(
+            self.framenote, text="Note Key", justify="center")
+        self.keyselector_text.grid(column=1, row=0)
+        
+    def add_instrument(self):
+        self.instrument_tree.insert('', 'end', text="Instrument %d" % (self.instrument_count) )
+        self.instrument_count += 1
+    
     def add_note(self):
-        self.instrument_tree.insert('Instr1', 'end', text=number_to_note(int(self.spinval.get())))
+        current_selection = self.instrument_tree.selection()[0]
+        
+        while (self.instrument_tree.parent(current_selection)):
+            current_selection = self.instrument_tree.parent(current_selection)
+        
+        self.instrument_tree.insert(current_selection, 'end', text=number_to_note(int(self.spinval.get())), tags=["note", self.note_count])
         self.note_count += 1
+        
+    
 
     def delete_note(self):
-        self.instrument_tree.delete('Instr1')
-
-    def funny_function(self):
-        print("Helloeggs")
+        self.instrument_tree.delete(self.instrument_tree.focus())
 
     def render(self):
+        asksaveasfile()
         print("renderrenderrender")
 
     def tree_select(self):
